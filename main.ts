711b63ce6d7d058b07dbe12eeccd3f66da61bced
Klaas Bosteels <klaas.bosteels@gmail.com>
2020-02-03 23:08:00 +0100

    Initial commit

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+
+pins.setPull(1, PinPullMode.PullUp);
+pins.setPull(2, PinPullMode.PullUp);
+pins.setPull(8, PinPullMode.PullUp);
+pins.setPull(16, PinPullMode.PullUp);
---
+++ nettendo_guswitch.ts
@@ -0,0 +1,32 @@
+//% weight=100 color=#00BBCC icon="\uF2DB" block="NG"
+namespace nettendo_guswitch {
+
+    export enum NGButton {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    export enum NGButtonPin {
+        //% block="Up (P1)"
+        Up = DAL.MICROBIT_ID_IO_P1,
+        //% block="Right (P2)"
+        Right = DAL.MICROBIT_ID_IO_P2,
+        //% block="Down (P8)"
+        Down = DAL.MICROBIT_ID_IO_P8,
+        //% block="Left (P16)"
+        Left = DAL.MICROBIT_ID_IO_P16,
+        //% block="A (P5)"
+        A = DAL.MICROBIT_ID_IO_P5,
+        //% block="B (P11)"
+        B = DAL.MICROBIT_ID_IO_P11,
+    }
+
+    //% blockId="ng_ispressed" block="button %button|is pressed"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    export function buttonIsPressed(button: NGButtonPin): boolean {
+        const pin = <DigitalPin><number>button;
+        return pins.digitalReadPin(pin) == 0;
+    }
+}
