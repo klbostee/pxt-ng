9b49ea8461180de8f3c3c4fda3e3a4b392060a57
Klaas Bosteels <klaas.bosteels@gmail.com>
2020-02-04 21:39:48 +0100

    Add button handlers

--- nettendo_guswitch.ts
+++ nettendo_guswitch.ts
@@ -1,12 +1,5 @@
 //% weight=100 color=#00BBCC icon="\uF2DB" block="NG"
-namespace nettendo_guswitch {
-
-    export enum NGButton {
-        Up,
-        Down,
-        Left,
-        Right,
-    }
+namespace ng {
 
     export enum NGButtonPin {
         //% block="Up (P1)"
@@ -23,11 +16,78 @@ namespace nettendo_guswitch {
         B = DAL.MICROBIT_ID_IO_P11,
     }
 
-    //% blockId="ng_ispressed" block="button %button|is pressed"
+    //% blockId="ng_ispressed" block="button %button is pressed"
     //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
     export function buttonIsPressed(button: NGButtonPin): boolean {
         const pin = <DigitalPin><number>button;
         pins.setPull(pin, PinPullMode.PullUp);
         return pins.digitalReadPin(pin) == 0;
     }
+
+    let buttonUpHandler: () => void = null;
+    let buttonRightHandler: () => void = null;
+    let buttonDownHandler: () => void = null;
+    let buttonLeftHandler: () => void = null;
+    let buttonAHandler: () => void = null;
+    let buttonBHandler: () => void = null;
+
+    //% block="on button %button pressed"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    export function onButtonPressed(button: NGButtonPin, handler: () => void) {
+        if (button == NGButtonPin.Up) {
+            buttonUpHandler = handler;
+        } else if (button == NGButtonPin.Right) {
+            buttonRightHandler = handler;
+        } else if (button == NGButtonPin.Down) {
+            buttonDownHandler = handler;
+        } else if (button == NGButtonPin.Left) {
+            buttonLeftHandler = handler;
+        } else if (button == NGButtonPin.A) {
+            buttonAHandler = handler;
+        } else if (button == NGButtonPin.B) {
+            buttonBHandler = handler;
+        }
+    }
+
+    let strip: neopixel.Strip = neopixel.create(DigitalPin.P12, 5, NeoPixelMode.RGB);
+
+    //% block
+    export function neopixels(): neopixel.Strip {
+        return strip;
+    }
+
+    //% block
+    export function startNettendoGuswitch() {
+        basic.clearScreen();
+
+        music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once);
+        basic.showIcon(IconNames.Heart);
+        basic.pause(500);
+        
+        strip.showColor(neopixel.colors(NeoPixelColors.Black));
+        strip.show();
+        
+        basic.forever(function () {
+            if (buttonIsPressed(NGButtonPin.Up) && buttonUpHandler != null) {
+                buttonUpHandler();
+            }
+            if (buttonIsPressed(NGButtonPin.Right) && buttonRightHandler != null) {
+                buttonRightHandler();
+            }
+            if (buttonIsPressed(NGButtonPin.Down) && buttonDownHandler != null) {
+                buttonDownHandler();
+            }
+            if (buttonIsPressed(NGButtonPin.Left) && buttonLeftHandler != null) {
+                buttonLeftHandler();
+            }
+            if (buttonIsPressed(NGButtonPin.A) && buttonAHandler != null) {
+                buttonAHandler();
+            }
+            if (buttonIsPressed(NGButtonPin.B) && buttonBHandler != null) {
+                buttonBHandler();
+            }
+            basic.clearScreen()
+        });
+    }
+
 }
