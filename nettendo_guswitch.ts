e8204da27e539e171238c78ca795c6419b3e856a
Klaas Bosteels <klaas.bosteels@gmail.com>
2020-02-12 22:54:47 +0100

    Add remotely-triggered events

--- nettendo_guswitch.ts
+++ nettendo_guswitch.ts
@@ -13,7 +13,24 @@ namespace ng {
         //% block="A (P5)"
         A = DAL.MICROBIT_ID_IO_P5,
         //% block="B (P11)"
-        B = DAL.MICROBIT_ID_IO_P11,
+        B = DAL.MICROBIT_ID_IO_P11
+    }
+
+    export enum NGButton {
+        //% block="Up (P1)"
+        Up = NGButtonPin.Up,
+        //% block="Right (P2)"
+        Right = NGButtonPin.Right,
+        //% block="Down (P8)"
+        Down = NGButtonPin.Down,
+        //% block="Left (P16)"
+        Left = NGButtonPin.Left,
+        //% block="A"
+        A = DAL.MICROBIT_ID_BUTTON_A,
+        //% block="B"
+        B = DAL.MICROBIT_ID_BUTTON_B,
+        //% block="A+B"
+        AB = DAL.MICROBIT_ID_BUTTON_AB
     }
 
     //% blockId="ng_ispressed" block="button %button is pressed"
@@ -25,44 +42,59 @@ namespace ng {
     }
 
     //% block="on button %button pressed"
-    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
-    export function onButtonPressed(button: NGButtonPin, handler: () => void) {
-        const pin = <DigitalPin><number>button;
-        pins.setPull(pin, PinPullMode.PullUp);
-        pins.setEvents(pin, PinEventType.Edge);
-        if (button == NGButtonPin.Up) {
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=4
+    export function onButtonPressed(button: NGButton, handler: () => void) {
+        if (button == NGButton.Up) {
+            const pin = <DigitalPin><number>button;
+            pins.setPull(pin, PinPullMode.PullUp);
+            pins.setEvents(pin, PinEventType.Edge);
             control.onEvent(
                 control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P1),
                 control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK),
                 handler
             );
-        } else if (button == NGButtonPin.Right) {
+        } else if (button == NGButton.Right) {
+            const pin = <DigitalPin><number>button;
+            pins.setPull(pin, PinPullMode.PullUp);
+            pins.setEvents(pin, PinEventType.Edge);
             control.onEvent(
                 control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P2),
                 control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK),
                 handler
             );
-        } else if (button == NGButtonPin.Down) {
+        } else if (button == NGButton.Down) {
+            const pin = <DigitalPin><number>button;
+            pins.setPull(pin, PinPullMode.PullUp);
+            pins.setEvents(pin, PinEventType.Edge);
             control.onEvent(
                 control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P8),
                 control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK),
                 handler
             );
-        } else if (button == NGButtonPin.Left) {
+        } else if (button == NGButton.Left) {
+            const pin = <DigitalPin><number>button;
+            pins.setPull(pin, PinPullMode.PullUp);
+            pins.setEvents(pin, PinEventType.Edge);
             control.onEvent(
                 control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P16),
                 control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK),
                 handler
             );
-        } else if (button == NGButtonPin.A) {
+        } else if (button == NGButton.A) {
             control.onEvent(
-                control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P5),
+                control.eventSourceId(EventBusSource.MICROBIT_ID_BUTTON_A),
                 control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK),
                 handler
             );
-        } else if (button == NGButtonPin.B) {
+        } else if (button == NGButton.B) {
             control.onEvent(
-                control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P11),
+                control.eventSourceId(EventBusSource.MICROBIT_ID_BUTTON_B),
+                control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK),
+                handler
+            );
+        } else if (button == NGButton.AB) {
+            control.onEvent(
+                control.eventSourceId(EventBusSource.MICROBIT_ID_BUTTON_AB),
                 control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK),
                 handler
             );
@@ -87,9 +119,44 @@ namespace ng {
         radio.setGroup(23);
         radio.sendNumber(0);
 
-        input.onGesture(Gesture.Shake, function () {
-            control.reset();
-        })
+        radio.onReceivedNumber(function (receivedNumber: number) {
+            if (receivedNumber == 1) {
+                control.raiseEvent(
+                    control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P1),
+                    control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK)
+                );
+            } else if (receivedNumber == 2) {
+                control.raiseEvent(
+                    control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P2),
+                    control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK)
+                );
+            } else if (receivedNumber == 8) {
+                control.raiseEvent(
+                    control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P16),
+                    control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK)
+                );
+            } else if (receivedNumber == 16) {
+                control.raiseEvent(
+                    control.eventSourceId(EventBusSource.MICROBIT_ID_IO_P8),
+                    control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK)
+                );
+            } else if (receivedNumber == 5) {
+                control.raiseEvent(
+                    control.eventSourceId(EventBusSource.MICROBIT_ID_BUTTON_A),
+                    control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK)
+                );
+            } else if (receivedNumber == 11) {
+                control.raiseEvent(
+                    control.eventSourceId(EventBusSource.MICROBIT_ID_BUTTON_B),
+                    control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK)
+                );
+            } else if (receivedNumber == 511) {
+                control.raiseEvent(
+                    control.eventSourceId(EventBusSource.MICROBIT_ID_BUTTON_AB),
+                    control.eventValueId(EventBusValue.MICROBIT_BUTTON_EVT_CLICK)
+                );
+            }
+        });
 
         kermis.onKermisNotePlayed(function () {
             strip.rotate(1);
@@ -139,6 +206,9 @@ namespace ng {
                 kermis.playSecondPartOfKermisChorus();
                 strip.showColor(neopixel.colors(NeoPixelColors.Black));
                 strip.show();
+            })
+            input.onButtonPressed(Button.AB, function () {
+                control.reset();
             })
             basic.clearScreen();
             let count = 0;
